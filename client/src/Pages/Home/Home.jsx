11b784ec2e6ed77bc44c9b5ab123a8a57057453b
Yubraj977 <yubrajkhatri977@gmail.com>
2024-04-30 01:45:32 -0700

    the submit message

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,26 +3,28 @@ import home from '/home.png'
 import Nav from '../../components/Nav'
 function Home() {
     return (
-        <div className='px-side bg-first text-white font-inter z-0 '>
-            <Nav/>
+        <div className='lg:px-side px-2 bg-first text-white font-inter z-0 overflow-x-hidden lg:block flex flex-col items-center'>
+            <Nav />
             <h1 className='font-semibold mt-20'>Create Account. Chat Nicely. Have Fun.</h1>
-            <div className=' w-4/5  '>
-                <h2 className=' font-semibold text-5xl mt-8 opacity-80'>Join our chat app, where conversations flourish and connections thrive!"  </h2>
-                <h2 className=' font-semibold text-5xl '>Sign up for find more  </h2>
+            <div className=' lg:w-4/5 w-full flex flex-col justify-center items-center lg:block '>
+             
+                <h2 className=' font-semibold  text-xl lg:text-5xl mt-4 text-center lg:text-left'>Join our chat app, where conversations flourish and connections thrive!"    </h2>
+                <h2 className=' font-semibold  text-3xl lg:text-5xl mt-4'>Sign up for find more   </h2>
+                <button className='bg-white text-black font-black px-8 rounded-md mt-10 lg:hidden py-2'>Login</button>
             </div>
 
-            <div className="textarea mt-10 flex gap-32  relative">
+            <div className="textarea mt-10 flex lg:gap-32 gap-10  relative lg:flex-row flex-col w-full items-center ">
 
-                <div className="left  w-1/5">
+                <div className="left  lg:w-1/5">
                     <p className='font-allerta '>Hello feel the pain</p>
                 </div>
-                <div className="center  w-1/5">
+                <div className="center  lg:w-1/5 text-center">
                     <h1 className='text-xl font-allerta'>This is second</h1>
                     <p className='opacity-25'>Lorem ipsum doloripsam nemovoluptates incidunt similique? Dolore aperiam pariatur obcaecati magnam officia eligendi nemo blanditiis!
                     </p>
 
                 </div>
-                <div className="right  w-1/5">
+                <div className="right  lg:w-1/5 text-center">
                     <h1 className='font-allerta text-xl'>This is second</h1>
                     <p className='opacity-25'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor exercitationem voluptatem fugit ipsam nemo, assumenda ratione sequi, laboriosam, voluptates incidunt similique? Dolore aperiam pariatur obcaecati magnam officia eligendi nemo blanditiis!
                     </p>
@@ -31,7 +33,7 @@ function Home() {
 
             </div>
             <div className='  '>
-            <img src={home} alt="home image" className='    w-full'  />
+                <img src={home} alt="home image" className='    w-full' />
             </div>
 
             <div className=''></div>
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,19 +3,19 @@ import { NavLink } from 'react-router-dom'
 import logo from '/logo.png'
 function Nav() {
     return (
-        <div className= '  bg-first text-white flex justify-between  py-4   items-center'>
+        <div className= '  bg-first text-white flex justify-between  py-4   items-center flex-col lg:flex-row  w-full'>
 
 
-            <div className='flex gap-2 items-center '>
-                <div className="left h-24  font-inter ">
+            <div className='flex gap-2 items-center  '>
+                <div className="left h-12 lg:h-24  font-inter ">
                     <img src={logo} alt="logo" className='h-full w-full object-cover' />
                 </div>
-                <h1 className='font-bold font-allerta text-3xl'>Chat Craft</h1>
+                <h1 className='font-bold font-allerta text-3xl '>Chat Craft</h1>
             </div>
 
-            <div className="right flex gap-3  justify-center items-center">
+            <div className="right flex gap-3  justify-center items-center   lg:w-auto w-full lg:mt-0 mt-10 ">
 
-                <NavLink className='font-bold text-2xl bg-teal-500 text-black  px-6 py-1 rounded-full' to='/login' >Login</NavLink>
+                <NavLink className=' lg:w-auto w-full  text-center font-bold text-2xl bg-white text-black rounded-md px-6 py-1'  to='/login' >Login</NavLink>
 
             </div>
         </div>
